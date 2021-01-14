5c9cb33d8d43932d99ae9adbca03d880c5bed22d
Nicky <54501296+Nick135ops@users.noreply.github.com>
2021-01-14 20:12:55 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,24 @@
+try:
+    from time import sleep
+    import random
+    import playsound
+    from datetime import datetime
+finally:
+    import os
+    os.system('python -3 -m pip install playsound')
+    os.system('python -3 -m pip install datetime')
+
+alarm_sounds= ['beat1.wav', 'music.mp3']
+print('Enter time in (HH:MM) format')
+wake_up_time = input('What time would you like to wake up?\n')
+curr_time = datetime.now().strftime("%H:%M")
+while 1:
+    if wake_up_time == curr_time:
+        print('Time to wake up!')
+        print('It\'s {0}'.format(curr_time))
+        playsound.playsound(random.choice(alarm_sounds))
+        break
+    else:
+        print('You still have enough time go to sleep again.')
+        print('Current time is {0}'.format(curr_time))
+        sleep(10)
